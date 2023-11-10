53154a69494c6fb5139dc7286d5941b22a6a13ed
ozgeboztass <123666735+ozgeboztass@users.noreply.github.com>
2023-11-10 18:24:57 +0300

    **

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -29,10 +29,10 @@ function Header() {
             </div>
 
             <form>
-              <div className="group  bg-gradient border-1 px-10 mx-4 py-2 ">
+              <div className="group text-sm bg-gradient border-1 px-10 mx-4 py-2 ">
                 <input
                   type="text "
-                  className="opacity-0 from-purple text-sm  to-blue rounded-[30px] shadow border border-t-blue border-l-blue border-b-purple border-r-purple border-opacity-50 group-hover:opacity-100 bg-transparent border-1 border-blue focus:outline-none w-40 lg:w-44 transition duration-500"
+                  className="opacity-0 from-purple text-sm  to-blue rounded-[30px] shadow border border-t-blue border-l-blue border-b-purple border-r-purple border-opacity-50 group-hover:opacity-100 bg-transparent border-1 border-blue focus:outline-none w-50 lg:w-60 transition duration-500"
                 />
                 <button className="-ml-4 group-hover:ml-0 duration-500 w-6 h-6">
                   <i className="fas fa-search text-purple group-hover:text-blue transition duration-500">
@@ -40,7 +40,7 @@ function Header() {
                       xmlns="http://www.w3.org/2000/svg"
                       height="0.75em"
                       viewBox="0 0 512 512"
-                      fill="white"
+                      fill="#5FC4E0"
                     >
                       <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
                     </svg>
@@ -54,7 +54,7 @@ function Header() {
                 Search
               </button>
 
-              <button className="flex items-center px-16 py-2 text-sm  md:text-sm border-1 border sm:hidden  rounded-[30px] m-auto border-t-blue border-l-blue border-b-purple border-r-purple bg-black  font-bold md:px-1 md:py-2  bg-primary hover:to-blue hover:from-darkpurple hover:text-gray transition duration-300">
+              <button className="flex items-center px-20 py-2  text-center text-xs  md:text-sm border-1 border sm:hidden  rounded-[30px] m-auto border-t-blue border-l-blue border-b-purple border-r-purple bg-black  font-bold md:px-1 md:py-2  bg-primary hover:to-blue hover:from-darkpurple hover:text-gray transition duration-300">
                 Connect Wallet
               </button>
             </div>
@@ -67,7 +67,7 @@ function Header() {
                 width={24}
                 height={24}
                 viewBox="0 0 24 24"
-                fill="none"
+                fill="white"
               >
                 <path
                   d="M3 8H21C21.2652 8 21.5196 7.89464 21.7071 7.70711C21.8946 7.51957 22 7.26522 22 7C22 6.73478 21.8946 6.48043 21.7071 6.29289C21.5196 6.10536 21.2652 6 21 6H3C2.73478 6 2.48043 6.10536 2.29289 6.29289C2.10536 6.48043 2 6.73478 2 7C2 7.26522 2.10536 7.51957 2.29289 7.70711C2.48043 7.89464 2.73478 8 3 8ZM21 16H3C2.73478 16 2.48043 16.1054 2.29289 16.2929C2.10536 16.4804 2 16.7348 2 17C2 17.2652 2.10536 17.5196 2.29289 17.7071C2.48043 17.8946 2.73478 18 3 18H21C21.2652 18 21.5196 17.8946 21.7071 17.7071C21.8946 17.5196 22 17.2652 22 17C22 16.7348 21.8946 16.4804 21.7071 16.2929C21.5196 16.1054 21.2652 16 21 16ZM21 11H3C2.73478 11 2.48043 11.1054 2.29289 11.2929C2.10536 11.4804 2 11.7348 2 12C2 12.2652 2.10536 12.5196 2.29289 12.7071C2.48043 12.8946 2.73478 13 3 13H21C21.2652 13 21.5196 12.8946 21.7071 12.7071C21.8946 12.5196 22 12.2652 22 12C22 11.7348 21.8946 11.4804 21.7071 11.2929C21.5196 11.1054 21.2652 11 21 11Z"
---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,91 @@
+import React from "react";
+import Header from "@/components/Header";
+
+function Home() {
+  return (
+    <>
+      <Header />
+
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              Before they sold out
+              <br className="hidden lg:inline-block" />
+              readymade gluten
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+              park mlkshk tote bag selvage hot chicken authentic tumeric
+              truffaut hexagon try-hard chambray.
+            </p>
+            <div className="flex justify-center">
+              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Button
+              </button>
+              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                Button
+              </button>
+            </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="https://dummyimage.com/720x600"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
+            <div className="w-full sm:p-4 px-4 mb-6">
+              <h1 className="title-font font-medium text-xl mb-2 text-gray-900">
+                Moon hashtag pop-up try-hard offal truffaut
+              </h1>
+              <div className="leading-relaxed">
+                Pour-over craft beer pug drinking vinegar live-edge gastropub,
+                keytar neutra sustainable fingerstache kickstarter.
+              </div>
+            </div>
+            <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+              <h2 className="title-font font-medium text-3xl text-gray-900">
+                2.7K
+              </h2>
+              <p className="leading-relaxed">Users</p>
+            </div>
+            <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+              <h2 className="title-font font-medium text-3xl text-gray-900">
+                1.8K
+              </h2>
+              <p className="leading-relaxed">Subscribes</p>
+            </div>
+            <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+              <h2 className="title-font font-medium text-3xl text-gray-900">
+                35
+              </h2>
+              <p className="leading-relaxed">Downloads</p>
+            </div>
+            <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+              <h2 className="title-font font-medium text-3xl text-gray-900">
+                4
+              </h2>
+              <p className="leading-relaxed">Products</p>
+            </div>
+          </div>
+          <div className="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
+            <img
+              className="object-cover object-center w-full h-full"
+              src="https://dummyimage.com/600x300"
+              alt="stats"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Home;
