619a6eebe405f97c35e7a4a1610952b3672f92e8
ozgeboztass <123666735+ozgeboztass@users.noreply.github.com>
2023-11-10 15:54:59 +0300

    **

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  plugins: [require("daisyui")],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    mytheme: {
+      primary: "#8f76e0",
+
+      secondary: "#e27fd7",
+
+      accent: "#577ec1",
+
+      neutral: "#1e1825",
+
+      "base-100": "#37383f",
+
+      info: "#4c65e1",
+
+      success: "#5ce0bd",
+
+      warning: "#d38509",
+
+      error: "#f37277",
     },
   },
   plugins: [],
-}
+};
