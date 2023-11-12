c4def0e72ec0d5b7b9c838a485984e31589c1747
ozgeboztass <123666735+ozgeboztass@users.noreply.github.com>
2023-11-12 18:36:26 +0300

    collections

--- my-app/src/pages/NftDetails.js
+++ my-app/src/pages/NftDetails.js
@@ -34,62 +34,54 @@ function NftDetails() {
                 <img src="./icons/user.svg" />
                 <span className="text-darkgray ml-1 mr-6">2.1K owners</span>
               </div>
-              <div class="overflow-x-auto text-sm text-[#D3D3D3]">
-                <table class="table-auto w-full">
+
+              <div className="overflow-x-auto text-sm text-[#D3D3D3] ">
+                <table className="table-auto w-full">
                   <tbody>
                     <tr>
-                      <td class="px-4 py-4">Year</td>
-                      <td class="px-4 py-4">2023</td>
+                      <td className="px-4 py-4">Year</td>
+                      <td className="px-4 py-4">2023</td>
                     </tr>
                     <tr>
-                      <td class="px-4 py-4">Dimensions:</td>
-                      <td class="px-4 py-4">40 x 50</td>
+                      <td className="px-4 py-4">Dimensions:</td>
+                      <td className="px-4 py-4">40 x 50</td>
                     </tr>
                     <tr>
-                      <td class="px-4 py-4">Blockchain:</td>
-                      <td class="flex items-center px-4 py-4">
-                        <img src="./icons/bnb.svg" class="h-4 mr-2" />
+                      <td className="px-4 py-4">Blockchain:</td>
+                      <td className="flex items-center px-4 py-4">
+                        <img src="./icons/bnb.svg" className="h-4 mr-2" />
                         <span>BNB Chain</span>
                       </td>
                     </tr>
                     <tr>
-                      <td class="px-4 py-4">Certificate of Authenticity:</td>
-                      <td class="px-4 py-4">Included</td>
+                      <td className="px-4 py-4">
+                        Certificate of Authenticity:
+                      </td>
+                      <td className="px-4 py-4">Included</td>
                     </tr>
                     <tr>
-                      <td class="px-4 py-4">Token ID:</td>
-                      <td class="px-4 py-4">1234</td>
+                      <td className="px-4 py-4">Token ID:</td>
+                      <td className="px-4 py-4">1234</td>
                     </tr>
                     <tr>
-                      <td class="px-4 py-4">Contract Address:</td>
-                      <td class="px-4 py-4">xx..x..x</td>
+                      <td className="px-4 py-4">Contract Address:</td>
+                      <td className="px-4 py-4">xx..x..x</td>
                     </tr>
                     <tr>
-                      <td class="px-4 py-4">Current Price:</td>
-                      <td class="px-4 py-4">**</td>
+                      <td className="px-4 py-4">Current Price:</td>
+                      <td className="px-4 py-4">**</td>
                     </tr>
                   </tbody>
                 </table>
               </div>
 
               <div className="flex">
-                <span className="title-font font-medium text-2xl text-white">
-                  $58.00
-                </span>
-                <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                  Button
-                </button>
-                <button className="rounded-full w-10 h-10 bg-gray-800 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
-                  </svg>
+                <div className="flex items-center title-font font-medium text-2xl text-[#D3D3D3]">
+                  <span>0.048ETH</span>
+                  <img src="./icons/48.svg" />
+                </div>
+                <button className="flex m-auto bg-gradient-to-r from-purple to-blue hover:to-blue hover:from-darkpurple text-darkpurple border-0 py-3 px-14 focus:outline-none rounded-3xl">
+                  CLAIM
                 </button>
               </div>
             </div>
@@ -108,88 +100,125 @@ function NftDetails() {
         </div>
       </section>
 
-      <section className="text-gray-600 body-font">
+      {/* card */}
+      <section className="text-gray-400 bg-gray-900 body-font">
         <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col w-full text-left">
+            <h1 className="sm:text-4xl text-3xl font-medium title-font mb-5 text-white">
+              Featured Collections
+            </h1>
+          </div>
           <div className="flex flex-wrap -m-4">
-            <div className="lg:w-1/4 sm:w-1/2 p-4">
-              <div className="flex relative">
+            <div className="p-4 md:w-1/3">
+              <div className="h-full border-[1px] border-t-blue border-l-blue border-b-purple border-r-purple  rounded-lg overflow-hidden">
                 <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src="https://dummyimage.com/601x361"
+                  className="lg:h-48 md:h-36 w-full object-cover object-center"
+                  src="./img/c1.png"
+                  alt="blog"
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white ">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    THE SUBTITLE
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    The Catalyzer
+                <div className="p-6">
+                  <h1 className="title-font text-xl text-center font-medium text-white mb-3">
+                    Being by Parin Heidari{" "}
+                    <img
+                      className="inline-block align-middle"
+                      src="./icons/blue.svg"
+                    />
                   </h1>
-                  <p className="leading-relaxed">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
-                  </p>
+
+                  <div className="flex justify-center items-center flex-wrap gap-x-4">
+                    <p className="text-sm text-gray flex-1 text-center">
+                      Floor
+                    </p>
+                    <p className="text-sm text-gray flex-1 text-center">
+                      24H Volume
+                    </p>
+                  </div>
+                  <div className="flex justify-center items-center flex-wrap gap-x-4">
+                    <p className="text-sl text-white flex-1 text-center">
+                      0.048 ETH
+                    </p>
+                    <p className="text-xl text-white flex-1 text-center">
+                      0.048 ETH
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="lg:w-1/4 sm:w-1/2 p-4">
-              <div className="flex relative">
+            <div className="p-4 md:w-1/3">
+              <div className="h-full border-[1px] border-t-blue border-l-blue border-b-purple border-r-purple  rounded-lg overflow-hidden">
                 <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src="https://dummyimage.com/601x361"
+                  className="lg:h-48 md:h-36 w-full object-cover object-center"
+                  src="./img/c2.png"
+                  alt="blog"
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white ">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    THE SUBTITLE
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    The Catalyzer
+                <div className="p-6">
+                  <h1 className="title-font text-xl text-center font-medium text-white mb-3">
+                    Monster Party{" "}
+                    <img
+                      className="inline-block align-middle"
+                      src="./icons/blue.svg"
+                    />
                   </h1>
-                  <p className="leading-relaxed">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
-                  </p>
+
+                  <div className="flex justify-center items-center flex-wrap gap-x-4">
+                    <p className="text-sm text-gray flex-1 text-center">
+                      Floor
+                    </p>
+                    <p className="text-sm text-gray flex-1 text-center">
+                      24H Volume
+                    </p>
+                  </div>
+                  <div className="flex justify-center items-center flex-wrap gap-x-4">
+                    <p className="text-sl text-white flex-1 text-center">
+                      0.048 ETH
+                    </p>
+                    <p className="text-xl text-white flex-1 text-center">
+                      0.048 ETH
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="lg:w-1/4 sm:w-1/2 p-4">
-              <div className="flex relative">
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white ">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    THE SUBTITLE
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    The Catalyzer
-                  </h1>
-                  <p className="leading-relaxed">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="lg:w-1/4 sm:w-1/2 p-4">
-              <div className="flex relative">
+
+            <div className="p-4 md:w-1/3">
+              <div className="h-full border-[1px] border-t-blue border-l-blue border-b-purple border-r-purple  rounded-lg overflow-hidden">
                 <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src="https://dummyimage.com/601x361"
+                  className="lg:h-48 md:h-36 w-full object-cover object-center"
+                  src="./img/c3.png"
+                  alt="blog"
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white ">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-                    THE SUBTITLE
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    The Catalyzer
+                <div className="p-6">
+                  <h1 className="title-font text-xl text-center font-medium text-white mb-3">
+                    /// by Snowfro{" "}
+                    <img
+                      className="inline-block align-middle"
+                      src="./icons/blue.svg"
+                    />
                   </h1>
-                  <p className="leading-relaxed">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
-                  </p>
+
+                  <div className="flex justify-center items-center flex-wrap gap-x-4">
+                    <p className="text-sm text-gray flex-1 text-center">
+                      Floor
+                    </p>
+                    <p className="text-sm text-gray flex-1 text-center">
+                      24H Volume
+                    </p>
+                  </div>
+                  <div className="flex justify-center items-center flex-wrap gap-x-4">
+                    <p className="text-sl text-white flex-1 text-center">
+                      0.048 ETH
+                    </p>
+                    <p className="text-xl text-white flex-1 text-center">
+                      0.048 ETH
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
+          </div>
+          <div className="flex mt-8 justify-center">
+            <img className="mr-10 h-10" src="./icons/Left.svg" />
+            <img className="h-10" src="./icons/Right.svg" />
           </div>
         </div>
       </section>
